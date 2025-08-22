022ec604329b916680b0781695fc1a01d0baa41f
Kapi-13 <rup.kacper.bo@gmail.com>
2025-08-22 10:00:45 +0200

    json

--- dist/js/main.js
+++ dist/js/main.js
@@ -78,7 +78,7 @@ window.onload = function (typeFirstMeal) {
             numFirstMeal = data.numFirstMeal;
             nextNumMeal = data.nextNumMeal;
         });
-    fetch("../config/Przepisy.json") //* Pobieranie z JSON danych
+    fetch("../config/przepisy.json") //* Pobieranie z JSON danych
         .then((res) => res.json()) //* Zmiana JSON w tablice obiektów js
         .then((data) => {
             switch (typeFirstMeal) {
